e744b6125791bfbe3be8a438125502e72f0e9d99
ipashchenko <in4pashchenko@gmail.com>
2019-03-20 19:24:08 +0300

    Add sorting posterior by flux. Tb works bad - tested.

--- postprocess_labels.py
+++ postprocess_labels.py
@@ -7,9 +7,22 @@ def get_r(sample, comp_length):
             range(int(length/comp_length))]
 
 
+def get_F(sample, comp_length):
+    length = len(sample)
+    return [(sample[i*comp_length+2]) for i in range(int(length/comp_length))]
+
+
 def sort_sample_by_r(sample, comp_length=4):
     r = get_r(sample, comp_length)
     indices = np.argsort(r)
+    # Construct re-labelled sample
+    return np.hstack([sample[i*comp_length: (i+1)*comp_length] for i in
+                      indices])
+
+
+def sort_sample_by_F(sample, comp_length=4):
+    F = get_F(sample, comp_length)
+    indices = np.argsort(F)[::-1]
     # Construct re-labelled sample
     return np.hstack([sample[i*comp_length: (i+1)*comp_length] for i in
                       indices])
@@ -19,4 +32,11 @@ def sort_samples_by_r(samples, comp_length=4):
     new_samples = list()
     for sample in samples:
         new_samples.append(sort_sample_by_r(sample, comp_length))
+    return np.atleast_2d(new_samples)
+
+
+def sort_samples_by_F(samples, comp_length=4):
+    new_samples = list()
+    for sample in samples:
+        new_samples.append(sort_sample_by_F(sample, comp_length))
     return np.atleast_2d(new_samples)
